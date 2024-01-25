7ff807b7ab82e1be95eb7126e5b3a69ea140a46c
- <->
2024-01-25 22:22:29 +0200

    fixed some changes using the diff tool

--- main.c
+++ main.c
@@ -15,34 +15,33 @@ int main() {
     scanf("%d", &num1);
     scanf("%d", &num2);
 
-    for (int i = num1; i <= num2; i++){
-        if (isPrime(i)){
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-
-
-    for (int i = num1; i <= num2; i++){
-        if (isStrong(i)){
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-
+    printf("The Armstrong numbers are:");
     for (int i = num1; i <= num2; i++){
         if (isArmstrong(i)){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\n");
 
+    printf("\nThe Palindromes are:");
     for (int i = num1; i <= num2; i++){
         if (isPalindrome(i)){
-            printf("%d ", i);
+            printf(" %d", i);
         }
     }
-    printf("\n");
 
+    printf("\nThe Prime numbers are:");
+    for (int i = num1; i <= num2; i++){
+        if (isPrime(i)){
+            printf(" %d", i);
+        }
+    }
+
+    printf("\nThe Strong numbers are:");
+    for (int i = num1; i <= num2; i++){
+        if (isStrong(i)){
+            printf(" %d", i);
+        }
+    }
+    printf("\n"); //in the example output files there are an extra line
     return 0;
 }
